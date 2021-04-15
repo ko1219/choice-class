067e527ac81c4710c088a2309de15a21dc912ece
ko1219 <73377107+ko1219@users.noreply.github.com>
2021-04-16 08:49:05 +0900

    update seeds.db

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,7 @@
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
 
-curriculums = Curriculum.create([
+Curriculum.create([
   {
     name: "English CommunicationI"
   },
