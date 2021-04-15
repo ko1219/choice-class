a6504bdd756a61852d048cea1fc4875182048473
ko1219 <73377107+ko1219@users.noreply.github.com>
2021-04-16 08:36:18 +0900

    empty commit

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import './App.css'
 import { Route, Switch } from "react-router-dom";
 import Curriculums from "./Curriculums/Curriculums";
 import Curriculum from "./Curriculum/Curriculum";
