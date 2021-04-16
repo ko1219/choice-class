ed24c2015f38f0bd1f1032a3c63a47679985d977
ko1219 <73377107+ko1219@users.noreply.github.com>
2021-04-16 11:26:46 +0900

    updated

--- app/javascript/packs/index.jsx
+++ app/javascript/packs/index.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "../components/App";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 document.addEventListener("DOMContentLoaded", () => {
   ReactDOM.render(
